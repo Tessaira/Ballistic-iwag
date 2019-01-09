e1e89027f635135939e81b06a995e58546e4f583
Tessaira <idawagenhofer9@gmail.com>
2019-01-09 15:33:11 +0100

    work 09.01.

---
+++ Assets/MyGame/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+// Lädt die neuen Szenen, wenn Game over ist, man den 'go again' Button klickt, oder das Spiel gestartet wird.
