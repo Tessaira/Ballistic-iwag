bfe74cbe72fabacabf8ef93a4bc80d87c4899e4d
Tessaira <idawagenhofer9@gmail.com>
2019-01-21 21:31:09 +0100

    added counter

---
+++ Assets/MyGame/Scripts/Counter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour {
+
+    private int hitCounter = 0;
+    [SerializeField] Text counterText;
+	
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            hitCounter++;
+            counterText.text = hitCounter.ToString();
+        }
+        
+    }
+}
